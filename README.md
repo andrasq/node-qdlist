e07c9fba09af3f658ea3f72e4424af21e78264db
Andras <andras@kinvey.com>
2019-01-05 15:35:23 -0500

    write tests for new methods, update readme

--- README.md
+++ README.md
@@ -9,27 +9,80 @@ Nodes are kept on a circular list, with the list itself being the terminal node.
 Api
 ---
 
+### qdlist( )
+
+Construct a new empty list.  Can be called either as a function or as a constructor with
+`new qdlist()`.
+
+    const qdlist = require('qdlist');
+    const list = qdlist();
+
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    list.moveToTail(list.head());
+    list.toArray();
+    // => [ 2, 3, 1 ]
+
 ### unshift( value )
 ### push( value )
 
-Add a new item to the head / tail of the list, respectively.  Returns the `node` that
-contains the item.
+Append a new value to the head / tail of the list, respectively.  Returns the `node` that
+contains the value.  The returned node has properties `value` containing the call argument
+and `value2` with initial contents not specified.
+
+### unshift2( value, value2 )
+### push2( value, value2 )
+
+Add a new pair of items to the head / tail of the list, respectively.  Returns the `node` that
+contains the item with properties `node.value` set to `value` and `node.value2` set to `value2`.
 
 ### shift( )
 ### pop( )
 
-Remove and return the first / last item from the list.  Returns `undefined` if the
-list is empty.
+Remove and return the first / last stored value from the list.  Returns `undefined` if the
+list is empty.  If necessary, use `isEmpty` to distinguish the `undefined` value from an
+empty list.
 
 ### isEmpty( )
 
-Test whether the list is empty.
+Returns boolean `true` if the list is empty, else `false`.
 
 ### unlink( node )
 
-Remove the specific node from the list.
+Remove the node from the list.
+
+### head( )
+### tail( )
+
+Return the first / last node on the list, or `undefined` if the list is empty.
+
+Note that head / tail / push / unshift return nodes, but shift / pop return values.
 
 ### moveToHead( node )
 ### moveToTail( node )
 
-Move the given node to the head / tail of the list.
+Move the given node to the head / tail of the list.  The node may be unlinked, or may still
+be on the list.
+
+### forEach( handler(node) )
+
+Call the handler with each node on the list, in list order.
+
+### toArray( [limit] )
+
+Return up to `limit` values from the list, in list order.  The default `limit` is Infinity,
+all values.  Useful for testing or inspection.
+
+    l.push(1);
+    l.push(2);
+    l.toArray();        // => [1, 2]
+
+Changelog
+---------
+
+- 0.0.1 - first working version
+
+
+Related Work
+------------
--- test-qdlist.js
+++ test-qdlist.js
@@ -60,6 +60,35 @@ module.exports = {
         t.done();
     },
 
+    'toArray': function(t) {
+        var l = qdlist();
+        t.deepEqual(l.toArray(), [ ]);
+        l.push(1);
+        t.deepEqual(l.toArray(), [ 1 ]);
+        l.unshift(2);
+        t.deepEqual(l.toArray(), [ 2, 1 ]);
+        l.push2(3, 4);
+        t.deepEqual(l.toArray(), [ 2, 1, 3 ]);
+        l.unshift2(4, 5);
+        t.deepEqual(l.toArray(), [ 4, 2, 1, 3 ]);
+        t.done();
+    },
+
+    'forEach': function(t) {
+        var l = qdlist();
+        var nodes = [];
+        var called = 0;
+        l.forEach(function(node) { called += 1 });
+        t.assert(!called);
+        nodes.push(l.push(0));
+        nodes.push(l.push(1));
+        nodes.push(l.push(2));
+        nodes.push(l.push(3));
+        l.forEach(function(node) { called += 1; t.equal(node, nodes[node.value]); });
+        t.equal(called, 4);
+        t.done();
+    },
+
     'shift': function(t) {
         var l = qdlist();
         l.push(1);
@@ -91,6 +120,34 @@ module.exports = {
         t.equal(l.next, l);
         t.equal(l.prev, l);
 
+        t.done();
+    },
+
+    'push2': function(t) {
+        var l = qdlist();
+        l.push2(1, 11);
+        l.push2(2, 22);
+        l.push2(3, 33);
+
+        t.equal(l.next.value2, 11);
+        t.equal(l.next.next.value2, 22);
+        t.equal(l.next.next.next.value2, 33);
+        t.equal(l.next.next.next.next, l);
+        
+        t.done();
+    },
+
+    'unshift2': function(t) {
+        var l = qdlist();
+        l.unshift2(3, 33);
+        l.unshift2(2, 22);
+        l.unshift2(1, 11);
+
+        t.equal(l.next.value2, 11);
+        t.equal(l.next.next.value2, 22);
+        t.equal(l.next.next.next.value2, 33);
+        t.equal(l.next.next.next.next, l);
+        
         t.done();
     },
 
@@ -136,9 +193,37 @@ module.exports = {
         t.done();
     },
 
+    'tail, moveToTail': function(t) {
+        var l = qdlist();
+        t.equal(l.tail(), undefined);
+        l.unshift(3);
+        l.unshift(2);
+        l.unshift(1);
+        t.equal(l.head().value, 1);
+        l.moveToTail(l.head());
+        t.equal(l.head().value, 2);
+        t.equal(l.tail().value, 1);
+        t.done();
+    },
+
+    'head, moveToHead': function(t) {
+        var l = qdlist();
+        t.equal(l.head(), undefined);
+        l.push(1);
+        l.push(2);
+        l.push(3);
+        t.equal(l.head().value, 1);
+        l.moveToHead(l.tail());
+        t.equal(l.head().value, 3);
+        t.equal(l.tail().value, 2);
+        t.done();
+    },
+
     'speed': function(t) {
         var l = qdlist();
+        if (process.env.NODE_COVERAGE === 'Y') return t.skip();
 
+for (var j=0; j<3; j++) {
         // for reproducible results restrict to one core, eg `taskset 2 qnit test*`
         var t1 = Date.now();
         for (var n=0; n<1e6; n++) {
@@ -146,7 +231,22 @@ module.exports = {
             for (var i=0; i<10; i++) l.pop();
         }
         var t2 = Date.now();
-        console.log("1e6 push/pop in %d ms", t2 - t1);
+        console.log("1e7 push/pop in %d ms", t2 - t1);
+
+        var t1 = Date.now();
+        for (var n=0; n<1e6; n++) {
+            for (var i=0; i<10; i++) l.push2(i, i);
+            for (var i=0; i<10; i++) l.pop();
+        }
+        var t2 = Date.now();
+        console.log("1e7 push2/pop in %d ms", t2 - t1);
+}
+
+        // note: node-v8 12% speed penalty for using the two-value form (but 5% node-v10, and no% node-v5.8)
+        // note: but only 2.5% penalty if only one struct is used throughout
+        // note: node-v10 60% slower than node-v8 and 100% slower than node-v5.8
+        l.push2(1, 2);
+        //console.log(l.head());
 
         t.done();
     },
